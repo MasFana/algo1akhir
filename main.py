fa77a76f4251fc5924201a4e511190eca0970e29
MasFana <adrianfathan69@gmail.com>
2023-11-26 21:05:11 +0700

    Add absensi data and check for duplicate absen

--- main.py
+++ main.py
@@ -149,6 +149,12 @@ def Daftar_produk():
 
 def Input_absen(username):
     waktu = time.localtime()
+    absensi = pd.read_csv("absensi.csv")
+    absenow = absensi[(absensi['username'] == username) & 
+                     (absensi['tanggal'] == waktu.tm_mday) & 
+                     (absensi['bulan'] == waktu.tm_mon) & 
+                     (absensi['tahun'] == waktu.tm_year)]
+    notabsen = absenow.empty
     waktu_dict = {
         "username":username,
         'jam':time.strftime("%H:%M:%S",waktu) ,
@@ -156,7 +162,10 @@ def Input_absen(username):
         'bulan':waktu.tm_mon,
         'tahun':waktu.tm_year}
     df = pd.DataFrame(waktu_dict,index=[0])
-    df.to_csv("absensi.csv",mode='a',header=False ,index=False)
+    if notabsen:
+        df.to_csv("absensi.csv",mode='a',header=False ,index=False)
+    else:
+        print(f"Anda sudah absen pada {absenow['jam'].to_string(index=False)}")
 
 def Update_produk():
     pdProduk = pd.read_csv("produk.csv")    
